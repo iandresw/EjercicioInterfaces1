5598faa57645f64f60f7979caadb10a7387aa06e
iandresw <rbejarano@unah.hn>
2024-05-13 20:18:39 -0600

    Update Program.cs
    se implemento la clase calcular para el calculo en este se verifico si no estaba nulo, vacio o si no era double caso contrario las operaciones no se podran realizar

--- Program.cs
+++ Program.cs
@@ -1,2 +1,44 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using EjercicioInterfaces;
+
+Console.WriteLine("******************BIENVENIDO******************");
+string? radio, angulo;
+Calculos calculos = new Calculos();
+Console.WriteLine("Ingrese el Radio del Circulo");
+radio =Console.ReadLine();
+Console.WriteLine("Ingrese el Angulo del circulo");
+angulo = Console.ReadLine();
+if (radio is null || radio == ""){
+    Console.WriteLine("Debe Ingredar un Radio Valido");
+    radio = Console.ReadLine(); 
+}else if (angulo is null || angulo == "")
+{
+    Console.WriteLine("Debe ingresar un angulo valido");
+    angulo = Console.ReadLine();
+
+}
+
+bool valRadio, valAngulo;
+valRadio = double.TryParse(radio, out calculos.radio);
+valAngulo = double.TryParse(angulo, out calculos.angulo);
+
+if (valRadio && valAngulo)
+{
+    calculos.Imprimir();
+}else if(valRadio is not true)
+{
+    Console.WriteLine("El Radio NO es Valido");
+    Console.WriteLine("No se pudo completar las operaciones");
+}
+else if(!valAngulo)
+{
+    Console.WriteLine("El Angulo NO es valido");
+    Console.WriteLine("No se pudo completar las operaciones");
+
+}
+else
+{
+    Console.WriteLine("No se pudo completar las operaciones");
+}
+
+
+Console.WriteLine("******************ES TODO******************");
